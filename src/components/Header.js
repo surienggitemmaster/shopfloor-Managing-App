7daa50fcc897e1560e5c0d0df55d7fd4c7240f8d
akash3106 <akash.sharma3106@gmail.com>
2024-08-07 00:09:30 +0530

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,6 @@ import React from 'react'
 import { signOut } from 'next-auth/react';
 
 function Header() {
-    const userEmail = "kamranmohd89@gmail.com"
-    const firstTwoLetters = userEmail.substring(0, 2).toUpperCase();
-
     return (
         <header className='sticky top-0 z-50 bg-white px-4 py-2 shadow-sm'>
             <div className='max-w-4xl mx-auto'>
